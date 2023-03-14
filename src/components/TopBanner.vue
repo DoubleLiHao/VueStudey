4f3d096d0c8377a3fd3c1a0886552030bffd7ba9
DoubleLiHao <=2998000782@qq.com>
2023-03-14 10:35:59 +0800

    事件总线

---
+++ src/components/TopBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TopBanner',
+    data() {
+        return {
+            title: '我是顶部栏'
+        }
+    }
+}
+</script>
+
+<style scoped>
+div{
+  color: white;
+}
+</style>
